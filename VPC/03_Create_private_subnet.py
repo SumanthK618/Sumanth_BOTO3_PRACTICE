ad68736750aa400cc179093d498d2e464c7d59b9
SumanthK618 <sumanthb672001@gmail.com>
2026-02-21 07:28:34 +0530

    Update 03_Create_private_subnet.py

--- VPC/03_Create_private_subnet.py
+++ VPC/03_Create_private_subnet.py
@@ -1,5 +1,5 @@
 '''
-Created one subnet for private so this subnet we are attaching any gateways....
+Created one subnet for private so this subnet we are not attaching any gateways....
 link:https://docs.aws.amazon.com/boto3/latest/reference/services/ec2/client/create_subnet.html#
 '''
 
